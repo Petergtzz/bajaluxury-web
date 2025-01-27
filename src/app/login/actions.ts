72d92b64d87e49032b707ac471f8bdebb453b0eb
Pedro Gutierrez <peter.gtz2020@gmail.com>
2025-01-27 09:01:10 -0700

    user can now verify against database

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -2,24 +2,30 @@
 
 import { z } from "zod";
 import { createSession, deleteSession } from "@/lib/session";
+import { client } from "@/lib/turso";
 import { redirect } from "next/navigation";
 
-const testUser = {
-  id: "1",
-  email: "contact@cosdensolutions.io",
-  password: "12345678",
-};
+// const testUser = {
+//   id: "1",
+//   email: "contact@cosdensolutions.io",
+//   password: "12345678",
+// };
 
 const loginSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }).trim(),
+  email: z
+    .string()
+    .toLowerCase()
+    .email({ message: "Invalid email address" })
+    .trim(),
   password: z
     .string()
-    .min(8, { message: "Password must be at least 8 characters" })
+    //.min(8, { message: "Password must be at least 8 characters" })
     .trim(),
 });
 
 export async function login(prevState: any, formData: FormData) {
   const result = loginSchema.safeParse(Object.fromEntries(formData));
+  let redirectPath: string | null = null;
 
   if (!result.success) {
     return {
@@ -29,17 +35,61 @@ export async function login(prevState: any, formData: FormData) {
 
   const { email, password } = result.data;
 
-  if (email !== testUser.email || password !== testUser.password) {
+  try {
+    const userQuery = await client.execute({
+      sql: "SELECT user_id, email, password, role FROM users WHERE email = ?",
+      args: [email],
+    });
+
+    console.log("userQuery", userQuery);
+
+    if (userQuery.rows.length === 0) {
+      return {
+        errors: {
+          email: ["Invalid email or password"],
+        },
+      };
+    }
+
+    const user = userQuery.rows[0];
+    console.log("user", user);
+
+    if (password !== user.password) {
+      return {
+        errors: {
+          email: ["Invalid email or password"],
+        },
+      };
+    }
+
+    await createSession(user.user_id);
+
+    redirectPath = `/dashboard`;
+  } catch (error) {
+    console.error("Login error:", error);
     return {
       errors: {
-        email: ["Invalid email or password"],
+        email: ["An error occurred. Please try again."],
       },
     };
+  } finally {
+    if (redirectPath) {
+      redirect(redirectPath);
+    }
   }
 
-  await createSession(testUser.id);
+  // Test user
+  // if (email !== testUser.email || password !== testUser.password) {
+  //   return {
+  //     errors: {
+  //       email: ["Invalid email or password"],
+  //     },
+  //   };
+  // }
 
-  redirect("/dashboard");
+  // await createSession(testUser.id);
+
+  // redirect("/dashboard");
 }
 
 export async function logout() {
